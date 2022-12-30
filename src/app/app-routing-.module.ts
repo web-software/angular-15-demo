570290eba15937f6115a588c4a17af48fccb102a
web-software <114349783+web-software@users.noreply.github.com>
2022-12-30 16:51:42 -0600

    2 files modified

--- src/app/app-routing-.module.ts
+++ src/app/app-routing-.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title: 'Dashboard',
+    path: 'dashboard',
+    component: HelloComponent,
+  },
+  {
+    title: 'Dashbaord',
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    title: 'Dashboard',
+    path: '**',
+    component: HelloComponent,
+  },
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
